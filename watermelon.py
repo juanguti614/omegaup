e1c7d788b926b019c6dbeecc7e008b8d84e91b64
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-08-27 14:36:01 -0500

    Add problem Watermelon

---
+++ watermelon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Watermel/#problems
+
+
+def _main() -> None:
+  # TODO: fixme
+  w = int(input())
+  if w % 2 == 0 and w > 2:
+    print("SI")
+  else:
+    print("NO")
+  
+
+if __name__ == '__main__':
+  _main()
