bddb76ff44f8063c18c76c06589892d79fc05391
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-09-01 21:03:52 -0500

    Add solution to problem dias_a_segundos

---
+++ dias_a_segundos.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Dias-a-segundos/
+
+def _main() -> None:
+  n = int(input())
+  d = 86400
+  s = n * d  
+  print(s)
+
+if __name__ == '__main__':
+  _main()
