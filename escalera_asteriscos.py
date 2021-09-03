31d50115757cc730964dbf9accb6a2df654d0fa6
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-09-02 19:15:23 -0500

    Add solution to problem escalera de asteriscos

---
+++ escalera_asteriscos.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Escalera-de-asteriscos/
+
+def _main() -> None:
+  n = int(input())
+  for i in range(n):
+      print ("#"*(i+1))
+
+if __name__ == '__main__':
+  _main()
