874205dacc5c9948be3bc2403b64e0f7a44a8fa0
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-09-06 17:55:54 -0500

    add solution to problem semana up

---
+++ semana_up.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Semana-UP/
+
+def _main() -> None:
+  a = int(input())
+  if a == 1:
+      print("lunes")
+  if a == 2:
+      print("martes")
+  if a == 3:
+      print("miercoles")
+  if a == 4:
+      print("jueves")
+  if a == 5:
+      print("viernes")
+  if a == 6:
+      print("sabado")
+  if a == 7:
+      print("domingo")
+
+if __name__ == '__main__':
+  _main()
