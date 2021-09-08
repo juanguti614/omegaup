0b383ba3743dfd20f108bbf2a74c1f54e9170c56
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-09-07 19:25:13 -0500

    add solution to problem alumnos_calificacion

---
+++ alumnos_calificacion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Alumnos-calificacion/
+
+def _main() -> None:
+  a = int(input()) * .60
+  b = int(input()) * .20
+  c = int(input()) * .20
+  tot = a + b + c
+  print (int(tot))
+
+
+if __name__ == '__main__':
+  _main()
