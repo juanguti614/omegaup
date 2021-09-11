16dfeea037151777e0f29531dd30061a6eb783fe
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-09-10 20:23:15 -0500

    add solution to histograma horizontal

---
+++ histograma_horizontal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Histograma_Horizontal/
+def _main() -> None:
+  a,b,c,d = input().split()
+  a = int(a)
+  b = int(b)
+  c = int(c)
+  d = int(d)
+  print ("*"*(a)) 
+  print ("*"*(b))  
+  print ("*"*(c)) 
+  print ("*"*(d)) 
+    
+
+if __name__ == '__main__':
+  _main()
