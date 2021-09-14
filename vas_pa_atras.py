3b1061f5a65cd4e163dd55015da5ffda878a3cf4
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-09-13 20:12:43 -0500

    Add solution to problem vas pa atras

---
+++ vas_pa_atras.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Vas-Pa-Atras/
+
+def _main() -> None:
+  n = int(input())
+  for i in range(n-1):
+    while n>0:  
+      print (n)
+      n = n-1
+if __name__ == '__main__':
+  _main()
