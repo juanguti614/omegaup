1b175ede31d0c25835b17a4d68ebe9d548c70116
Juan Diego Gutierrez <suanelo.2003@gmail.com>
2021-09-20 19:47:21 -0500

    Add solution to problem bajando caracteres

---
+++ bajando_caracteres.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# https://omegaup.com/arena/problem/Bajando-los-caracteres/
+def _main() -> None:
+  a = str(input())
+  for i in range (len(a)):
+    print (a[i])
+if __name__ == '__main__':
+  _main()
